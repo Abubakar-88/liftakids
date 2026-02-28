046be23a8f0febdb13a01f37a75d4c99ca640342
Abu Bakar <siddique0153@gmail.com>
2026-02-28 13:03:50 +0600

    bug fix

---
+++ src/main/java/org/liftakids/exception/DataTruncationException.java
@@ -0,0 +1,7 @@
+package org.liftakids.exception;
+
+public class DataTruncationException extends RuntimeException {
+    public DataTruncationException(String message) {
+        super(message);
+    }
+}
